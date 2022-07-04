5ef2e99b837be60d5c094ad286cd3f70fb525a1c
ARINDA MARK <marcarinda@yahoo.com>
2022-07-04 10:00:32 -0700

    fix

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -8,11 +8,12 @@
 int main(void)
 
 {
-int ch;
-for (ch =97; ch <= 122; ch++)
-{
-putchar(ch);
-}
-putchar(10);
-return (0);
+	int ch;
+	for (ch =97; ch <= 122; ch++)
+	{
+	putchar(ch);
+	}
+	putchar(10);
+
+	return (0);
 }
