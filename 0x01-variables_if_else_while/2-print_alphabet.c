b5f10f658705c4eae866faaa386ba640fc5fae1b
ARINDA MARK <marcarinda@yahoo.com>
2022-07-04 18:08:52 -0700

    digits

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * return : always 0
+ */
+int main(void)
+{
+	int ch;
+	for (ch = 97; ch <= 122; ch++)
+	{
+	putchar(ch);
+	}
+	putchar(10);
+
+return (0);
+}
