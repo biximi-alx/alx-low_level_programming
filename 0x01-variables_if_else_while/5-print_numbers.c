6521da3d8257d8e41003a1a22bf384e39096eac5
Mark Arinda <107573006+biximi-alx@users.noreply.github.com>
2022-07-05 04:52:30 +0300

    Update 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - a program that prints the alphabet in lowercase, and then in uppercase
+ *
+ *return: success always 0
+ */
+int main(void)
+{
+  int n;
+  for(n =0; n<10;n++)
+  {
+    printf("%d\n", n);
+  }
+ 
+  return (0);
+}
