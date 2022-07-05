48714c9eae050cf0ded8595589a8e6a9cc3c4a07
ARINDA MARK <marcarinda@yahoo.com>
2022-07-04 19:34:56 -0700

    character

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - a program that prints all single digit numbers 
+ * return: return always 0
+ */
+int main(void)
+{
+int ch;
+for (n = 0; n < 10; n++)
+{
+printf("%d", n);
+}
+printf("\n");
+return (0);
+}
