fe05bbf21ea0f91ca5ded75260058357eba11839
Mark Arinda <107573006+biximi-alx@users.noreply.github.com>
2022-07-06 05:20:03 +0300

    Update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-* _islower -> checks if the character is lowercase
+* _islower - checks if the character is lowercase
 * @c: a charcter argument
 * Return: returns 1 and 0 depending on condition
 */
