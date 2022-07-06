ae0c45b1e2a12eb40bea1c57eca6be2b48286523
Mark Arinda <107573006+biximi-alx@users.noreply.github.com>
2022-07-06 09:18:19 +0300

    a program that prints all single digit numbers of base 10 starting from 0, followed by a new line

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
 
 /**
- * main - a program that prints the alphabet in lowercase, and then in uppercase
- *
- *return; always return 0
+ *main -> assign a random number to the variable n each time it is executed
+ *and print the last digit of the number stored in the variable n
+ *Return: Always 0 (Success)
  */
 int main(void)
 {
-	int a =0;
-	for(; a < 10 ; a++)
-	printf("%d\n", a);
-	putchar('\n');
-	return (0);
+int n;
+
+for (n = 0; n < 10; n++)
+{
+printf("%d", n);
+}
+printf("\n");
+return (0);
 }
