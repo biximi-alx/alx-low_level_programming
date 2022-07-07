9f416a8ade3b0ca830623379f2a74d1786ec4118
Mark Arinda <107573006+biximi-alx@users.noreply.github.com>
2022-07-07 21:18:07 +0300

    a function that checks for a digit (0 through 9)

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+*_isdigit - checkes if the no. is b/n 0 to 9
+*@m: input no
+*Return: 1 if is a number (0 to 9),0 in other case.
+*/
+int _isdigit(int m)
+{
+  if (m >= 48 && m <= 57)
+  {
+  return (1);
+  }
+  else
+  {
+  return (0);
+  }
+  -putchar('\n');
+}  
