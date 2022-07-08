8cb80249022a759942e1d2fac49a7fb14f686541
ARINDA MARK <marcarinda@yahoo.com>
2022-07-08 10:18:07 -0700

    fit

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -14,8 +14,8 @@ void print_diagonal(int n)
 	{
 		for (x = 0; x < n; x++)
 		{
-			for(i = 0; i <= x; i ++)
-				putchar(' ');
+			for(i = 0; i <= x; i++)
+				putchar (' ');
 			putchar(92);
 			putchar('\n');
 		}
