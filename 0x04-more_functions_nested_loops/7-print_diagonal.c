a99b8d9fd9ba8a93e7973c2354180465b17c48b1
ARINDA MARK <marcarinda@yahoo.com>
2022-07-08 06:45:17 -0700

    vyt

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_diagonal - printing diagonal
+ * @size: integer
+ */
+void print_diagonal(int size)
+{
+	int x;
+
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
+		for (x = 0; x < size; x++)
+		{
+			for(i = 0; i <= x; i ++)
+				putchar(' ');
+			putchar(92);
+			putchar('\n');
+		}
+		_putchar('\n');
+	}
+}
