fd2e610791c4241bca06d0420788244da9e6b1a2
ARINDA MARK <marcarinda@yahoo.com>
2022-07-07 23:56:51 -0700

    kit

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include "main.h"
 
 /**
@@ -15,21 +14,3 @@ void print_most_numbers(void)
 	}
 	_putchar('\n');
 }
-=======
-#include 'main.h"
-
-/**
-* print_most_numbers - the function ptints most numbers
-*/
-void print_most_numbers(void)
-{
-  int x;
-  
-  for (x = 0; x < 10; x++)
-  {
-    if (x !=2 && x !=4
-      _putchar(x + '0');
-  }
-  _putchar('\n');
-}
->>>>>>> ce59d1fdb6500872658c0b7b00c070f1df2717d7
