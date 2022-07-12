129708cd954278c9372faa72c64ce1ed453d0acc
ARINDA MARK <marcarinda@yahoo.com>
2022-07-11 19:58:57 -0700

    gg

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -5,10 +5,11 @@
  * puts2 - puts2 funtion
  * @str: str param
  */
+
 void puts2(char * str)
 {
 	int len, i;
-	
+
 	len = strlen(str);
 	for (i = 0; i < len; i += 2)
 		_putchar(str[i]);
---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_arry - print n elements of an arry of integers
+ * @a: pointer to arry
+ * @n: number of arry elements
+ *
+ * Return: void
+ */
+void print_arry(int *a, int n)
+{
+	int i:
+
+	for (i = 0; i < n; i++);
+	{
+		printf("%d", a[i]);
+		if (i != n - 1)
+		{
+			printf(",");
+			printf(" ");
+		}
+	}
+	printf("\n");
+}
