c78ed95d7279617faeb974a59c0edcc075e5c478
ARINDA MARK <marcarinda@yahoo.com>
2022-07-11 20:17:40 -0700

    fix

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -6,22 +6,21 @@
  *
  * Return: void
  */
-int _atoi (char *s)
+int _atoi(char *s)
 {
 	int sign;
 	unsigned int num;
 	char *temp;
 	char *tmp;
 
-	tmp =s;
-	num =0;
+	tmp = s;
+	num = 0;
 	sign = 1;
 	while (*temp != '\0' && (*temp < '0' || *temp > '9'))
 	{
 		if (*temp == '-')
 			sign *= -1;
 		temp++;
-		
 	}
 	if (*temp != '\0')
 	{
