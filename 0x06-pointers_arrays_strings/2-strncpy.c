99ffc03f7a2c5a988d088122afc1c434fa185730
ARINDA MARK <marcarinda@yahoo.com>
2022-07-13 15:01:45 -0700

    string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strncpy - for copying 
+ * @dest: parameter
+ * @src: parameter
+ * @n: parameter
+ */
+char *_strcat(char *dest, char *src)
+{
+	strncpy(dest, src, n);
+	return (dest);
+}
