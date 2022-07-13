bed8075ca30da0e31e17462327338463e72be07f
ARINDA MARK <marcarinda@yahoo.com>
2022-07-13 16:09:58 -0700

    gir

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -5,13 +5,13 @@
  * @x: string param
  * Return: string
  */
-char *string_toupper(char *)
+char *string_toupper(char *x)
 {
 	int i = 0;
 
 	while (x[i])
 	{
-		if (x[i] >= 97 && x[i] <=122)
+		if (x[i] >= 97 && x[i] <= 122)
 			x[i] = x[i] - 32;
 		i++;
 	}
