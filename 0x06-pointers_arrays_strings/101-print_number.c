66f465210d946729b803860b3e980dbb8c2441bc
ARINDA MARK <marcarinda@yahoo.com>
2022-07-14 00:53:41 -0700

    dttt

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -15,7 +15,7 @@ void print_number(int n)
 	hold = n;
 	if (n < 0)
 	{
-		_putchar('-');
+		putchar('-');
 		neg = 1;
 	}
 	while (hold > 9 || hold < -9)
@@ -28,18 +28,18 @@ void print_number(int n)
 		if (power > 9)
 		{
 			if (!neg)
-				_putchar((n / power % 10) + '0');
+				putchar((n / power % 10) + '0');
 			else
-				_putchar((n / power % 10) * -1 + '0');
+				putchar((n / power % 10) * -1 + '0');
 
 			power /= 10;
 		}
 		if (power == 1)
 		{
 			if (neg)
-				_putchar((n % 10) * -1 + '0');
+				putchar((n % 10) * -1 + '0');
 			else
-				_putchar(n % 10 + '0');
+				putchar(n % 10 + '0');
 			power = 0;
 		}
 	}
