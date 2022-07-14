58761a90e8f63d3bc72ffabb006b3febecef8d00
ARINDA MARK <marcarinda@yahoo.com>
2022-07-13 19:18:14 -0700

    src

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * reverse_array - reversing an array
+ * @a: array a
+ * @n: an element of an array
+ */
+void reserse_array(int *a, int n)
+{
+	int *p, i, aux, k;
+
+	p = a;
+	for (i = 0; i < n; i++)
+		p++;
+	for (k = 0; k < n / 2; k++)
+	{
+		aux = a[k];
+		a[k] = *p;
+		*p = aux;
+		p--;
+	}
+}
