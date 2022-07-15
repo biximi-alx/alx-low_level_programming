591a766c9af554d2939025fc8e8fcb3b8cce9db5
ARINDA MARK <marcarinda@yahoo.com>
2022-07-14 19:33:09 -0700

    ddr

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,4 +1,5 @@
 #include "main.h"                                                                                          
+
 /**                                                                                                        
  * infinite_add - C function that adds two numbers stored                                                  
  * in strings to a buffer                                                                                  
@@ -30,29 +31,29 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
  *         if r cannot store the sum - 0
  */
  
- char *add_strings(char *n1, char *n2, char *r, int r_index)
- {
-         int num, tens = 0;
-         
-         for (; *n1 && *n2; n1--, n2--, r_index--)
-         {
-                 num = (*n1 - '0') + (*n2 - '0');
-                 num += tens;
-                 *(r + r_index) = (num % 10) + '0';
-                 tens = nnum / 10;
-         }
-         
-         for (; *n1; n1--; r_index++)
-         {
-                 num = *(n1 - '0') + tens;
-                 *(r + r_index) = (num % 10) + '0';
-                 tens = nnum / 10;
-         }
-         
-         for (; *n2; n2--; r_index++)
-         {
-                 num = *(n2 - '0') + tens;
-                 *(r + r_index) = (num % 10) + '0';
-                 tens = nnum / 10;
-         }
- }
+char *add_strings(char *n1, char *n2, char *r, int r_index)
+{
+	int num, tens = 0;
+	
+	for (; *n1 && *n2; n1--, n2--, r_index--)
+	{
+		num = (*n1 - '0') + (*n2 - '0');
+		num += tens;
+		*(r + r_index) = (num % 10) + '0';
+		tens = nnum / 10;
+	}
+	
+	for (; *n1; n1--; r_index++)
+	{
+		num = *(n1 - '0') + tens;
+		*(r + r_index) = (num % 10) + '0';
+		tens = nnum / 10;
+	}
+	
+	for (; *n2; n2--; r_index++)
+	{
+		num = *(n2 - '0') + tens;
+		*(r + r_index) = (num % 10) + '0';
+		tens = nnum / 10;
+	}
+}
