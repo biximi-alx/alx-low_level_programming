e8c15403bcc07c158aaec323e64662cebe499bf4
ARINDA MARK <marcarinda@yahoo.com>
2022-07-14 19:39:00 -0700

    ddr

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -47,7 +47,7 @@ char *add_strings(char *n1, char *n2, char *r, int r_index)
 	{
 		num = *(n1 - '0') + tens;
 		*(r + r_index) = (num % 10) + '0';
-		tens = nnum / 10;
+		tens = num / 10;
 	}
 	
 	for (; *n2; n2--; r_index++)
