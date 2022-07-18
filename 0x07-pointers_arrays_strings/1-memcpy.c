acb98c732883cf64c92bd847c8504214b57cf52a
ARINDA MARK <marcarinda@yahoo.com>
2022-07-18 12:12:58 -0700

    Task-1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -10,11 +10,9 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int a:
+	unsigned int a;
 
 	for (a = 0; a < n; a++)
-	{
-		dest[a] = src[a]:
-	}
-	return (dest);
+	dest[a] = src[a];
+		return (dest);
 }
