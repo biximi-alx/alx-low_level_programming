a42e6a87c1c0cfb6a1d35ea71e7052502a2efaae
ARINDA MARK <marcarinda@yahoo.com>
2022-07-20 07:43:56 -0700

    Task-5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,4 +1,28 @@
 #include "main.h"
 
 /**
- *
+ * _sqrt_recursion - squareroot using recursion
+ * @n: parameter to be sqaureroot
+ * Return: squareroot of a number
+ */
+int _sqrt_recursion(int n)
+{
+	return (_sqrt(n, 1));
+}
+
+/**
+ * _sqrt - recursive squareroot
+ * @n: number
+ * @i: interator
+ * Return: a number
+ */
+int _sqrt(int n, int i)
+{
+	int square = i * i;
+
+	if (square > n)
+		return (-1);
+	if (square == n)
+		return (i);
+	return (_sqrt(n, i + 1));
+}
