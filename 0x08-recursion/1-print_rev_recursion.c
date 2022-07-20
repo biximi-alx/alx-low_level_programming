17b3815642ab353ee33f0b4d0222c5641cf40c4f
ARINDA MARK <marcarinda@yahoo.com>
2022-07-20 06:47:17 -0700

    Task-1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,16 +1,17 @@
 #include "main.h"
 
 /**
- * _puts_recursion - print a string in reverse
+ * _print_rev_recursion - print a string in reverse
  * @s: pointer block of memory to fill
  *
  * Return: void
  */
 void _print_rev_recusion(char *s)
 {
-	if (*s)
-	{
-	_print_rev_recursion(s + 1);
+	if (*s == '\0')
+		return;
+	s++;
+	_print_rev_recursion(s);
+	s--;
 	_putchar(*s);
-	}
 }
