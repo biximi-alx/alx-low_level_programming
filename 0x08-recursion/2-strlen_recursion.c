9aa16c3c75d847d3004f1b7b5e346ecab414808b
ARINDA MARK <marcarinda@yahoo.com>
2022-07-20 06:55:37 -0700

    Task-2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - function to get length of string
+ * @s: string parameter
+ * Return: length pf string
+ */
+int _srtlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	s++;
+	return (1 + _strlen_recursdion(s));
+}
