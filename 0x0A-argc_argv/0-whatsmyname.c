ba333583eda26329048ffd8f0a33319939500d9d
ARINDA MARK <marcarinda@yahoo.com>
2022-07-22 14:42:16 -0400

    AA

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - this is a function to print its name
+ * @argc: argc parameter
+ * @argv: an array of a command listed
+ * Return: on success
+ */
+int main(int arg, char *argv[])
+{
+	printf("%s\n", *argv);
+}
