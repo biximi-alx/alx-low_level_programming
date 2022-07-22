ba4a99ea9e32f5ad66bda1301e536995d46fda25
ARINDA MARK <marcarinda@yahoo.com>
2022-07-22 19:51:54 -0400

    AA

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,9 +4,9 @@
  * main - this is a function to print its name
  * @argc: argc parameter
  * @argv: an array of a command listed
- * Return: on success
+ * Return: 0 on success
  */
-int main(int argc _attribute_((unused)), char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", *argv);
 	return (0);
