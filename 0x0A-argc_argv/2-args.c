3ab99fd4afb66b29786130bb93f7ea8450eae6b0
ARINDA MARK <marcarinda@yahoo.com>
2022-07-22 20:17:57 -0400

    AC

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -8,11 +8,11 @@
  */
 int main(int argc, char *argv[])
 {
-	int 1;
+	int i;
 
 	for (i = 0; i < argc; i++)
 	{
-		printf("argv index %d is = %s\n", argv[i]);
+		printf("%s\n", argv[i]);
 	}
 	return (0);
 }
