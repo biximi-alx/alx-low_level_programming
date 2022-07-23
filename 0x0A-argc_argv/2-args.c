e589449d70377c014e3c0a1b5a07f705dd226ad3
ARINDA MARK <marcarinda@yahoo.com>
2022-07-22 20:11:55 -0400

    AC

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio>
+
+/**
+ * main - this is a function to print its name
+ * @argc: parameter
+ * @argv: an array of a command listed
+ * Return: 0 on success
+ */
+int main(int argc, char *argv[])
+{
+	int 1;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("argv index %d is = %s\n", i, argv[i]);
+	}
+	return (0);
+}
