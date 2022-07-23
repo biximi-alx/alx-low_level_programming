f48cc18f2403a2b6923b14210c097774821827ca
ARINDA MARK <marcarinda@yahoo.com>
2022-07-23 04:45:22 -0400

    A4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -11,10 +11,10 @@ void print_most_numbers(void)
 	for (number = 48; number < 58; number++)
 	{
 		if ((number == 50) || (number == 52))
-	}
-	continue;
-	{
-	_putchar(number);
+		{
+			continue;
+		}
+		_putchar(number);
 	}
 	_putchar(10);
 }
