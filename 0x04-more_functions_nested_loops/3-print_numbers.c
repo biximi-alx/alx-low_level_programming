4ea2df39fea0e3f501f561b78d7d44203b3c8f29
ARINDA MARK <marcarinda@yahoo.com>
2022-07-23 02:58:45 -0400

    A3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - print numbers
+ * Return: Aways 0 (success)
+ */
+void print_numbers(void)
+{
+	int i;
+
+	i = 0;
+	{
+		_putchar (i + '0');
+		i++;
+	}
+	_putchar ('\n');
+}
