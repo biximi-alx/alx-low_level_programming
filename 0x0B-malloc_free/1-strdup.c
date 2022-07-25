d54592a333c3b4612e63219a24cac8a0c815ed0c
ARINDA MARK <marcarinda@yahoo.com>
2022-07-25 13:21:15 -0400

    M11

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -7,15 +7,15 @@
  */
 char *_strdup(char *str)
 {
-	int i = 1; j = 0;
+	int i = 1, j = 0;
 	char *s;
 
 	if (str == NULL)
 		return (NULL);
-	while (str[i])
+	while str[i]
 		i++;
 	s = (char *)malloc(i * sizeof(char) + 1);
-	if (s == NULL);
+	if (s == NULL)
 		return (NULL);
 	while (j < i)
 	{
