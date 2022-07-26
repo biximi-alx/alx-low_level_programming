327b8808b30f5ee62eb41fbc7be7040790146392
Mark Arinda <107573006+biximi-alx@users.noreply.github.com>
2022-07-26 14:19:36 -0400

    A7

--- 0x09-static_libraries/1-strncat.c
+++ 0x09-static_libraries/1-strncat.c
@@ -1,34 +1,34 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * _atoi - a function that converts string to integer
- * @s: An input string
- * Return: integer from conversion
+ * _strncat - a function that concatenates two strings.
+ * @dest: an input string
+ * @src: an input string
+ * @n: an input integer
+ * Return: A pointer to the resulting string
  */
-int _atoi(char *s)
+char *_strncat(char *dest, char *src, int n)
 {
-	int sign = 1;
-	unsigned int total = 0;
-	char null_flag = 0;
+	int srclen = 0, i = 0;
+	char *temp = dest, *start = src;
 
-	while (*s)
+	while (*src)
 	{
-		if (*s == '-')
-			sign *= -1;
-
-		if (*s >= '0' && *s <= '9')
-		{
-			null_flag = 1;
-			total = total * 10 + *s - '0';
-		}
-
-		else if (null_flag)
-			break;
-		s++;
+		srclen++;
+		src++;
 	}
 
-	if (sign < 0)
-		total = (-total);
+	while (*dest)
+		dest++;
 
-	return (total);
+	if (n > srclen)
+		n = srclen;
+
+	src = start;
+
+	for (; i < n; i++)
+		*dest++ = *src++;
+
+	*dest = '\0';
+	return (temp);
 }
