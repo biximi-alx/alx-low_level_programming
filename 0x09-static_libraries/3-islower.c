1a7924baff06274fd20939abc85095a3559f2a15
Mark Arinda <107573006+biximi-alx@users.noreply.github.com>
2022-07-26 14:25:29 -0400

    A12

--- 0x09-static_libraries/3-islower.c
+++ 0x09-static_libraries/3-islower.c
@@ -1,11 +1,22 @@
 #include "main.h"
 
 /**
-* _islower - checks.
-* @c: a charcter argument.
-* Return: returns 1 and 0 depending on condition.
-*/
+ * _islower - Check Holberton
+ * @c: An input character
+ * Description: function uses _putchar function to print
+ * alphabet in lowercase 10 times
+ * Return: 1 if is lowercase or 0 if is uppercase
+ */
 int _islower(int c)
 {
-	return (c >= 'a' && c <= 'z');
+	char i;
+	int lower = 0;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		if (i == c)
+			lower = 1;
+	}
+
+	return (lower);
 }
