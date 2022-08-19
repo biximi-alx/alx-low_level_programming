43051a9d09f30e9a2f384a3fbccf12186cf537ea
MARK ARINDA <marcarinda@yahoo.com>
2022-08-18 20:24:48 -0400

    Task1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,7 +7,7 @@
  * @n: number to convert to binary
  * You are not allowed to use arrays
  * You are not allowed to use malloc
- * You are not allowed to use the % or / operators
+ * You are not allowed to use the %  or / operators
  */
 void print_binary(unsigned long int n)
 {
