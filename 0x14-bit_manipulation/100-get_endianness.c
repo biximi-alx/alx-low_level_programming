8b946341f97fa895ed7bcf9f2dd8d42a5f737313
MARK ARINDA <marcarinda@yahoo.com>
2022-08-18 20:36:58 -0400

    Task6

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * get_endianness - Write a function that checks the endianness.
+ * get_endianness - Write  a function that checks the endianness.
  * Prototype: int get_endianness(void).
  * Return: 1, if architecture is little endian, 0 in case of big endian.
  */
